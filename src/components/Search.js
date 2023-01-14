4b2cebf7752f457a43622690b8d823bd56ec645e
ShafiArnob <shafiarnob@gmail.com>
2023-01-14 20:18:48 +0600

    search bar icons and search bar clear

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { mockSearchResults } from '../constants/mock'
 
+import {XIcon, SearchIcon} from "@heroicons/react/solid"
 const Search = () => {
   const [input, setInput] = useState("")
   const [bestMatches, setBestMatches] = useState(mockSearchResults.result)
@@ -17,6 +18,16 @@ const Search = () => {
   return (
     <div className='flex items-center my-4 border-2 rounded-md relative z-50 w-96 bg-white border-neutral-200'>
       <input onChange={(e)=>{setInput(e.target.value)}} onKeyPress={(e)=>{updateBestMatches()}} type="text" value={input} className="w-full px-4 py-2 focus:outline-none rounded-md" placeholder='Search Stock...'/>
+    
+      {input &&( 
+        <button onClick={clear}>
+          <XIcon className="h-4 w-4 fill-gray-500"/>
+        </button>)}
+
+      <button onClick={updateBestMatches} className="h-8 w-8 mx-1 bg-indigo-600 rounded-md flex justify-center items-center">
+        <SearchIcon className='h-4 w-4 fill-gray-100'/>
+      </button>
+
     </div>
   )
 }
